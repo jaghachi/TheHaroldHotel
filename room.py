65d5af430090f44b349b0e7ddd02f4f3f7ede235
Macy Varga <macylain@outlook.com>
2024-07-23 16:03:04 -0700

    comments for other people to understand code xoxox

--- room.py
+++ room.py
@@ -3,45 +3,56 @@ from typing import List
 
 class Room:
     def __init__(self, room_id: int, price: int, persons: int, date_available_from: date, hotel_name: str, city_name: str):
-        self.room_id = room_id
-        self.price = price
-        self.persons = persons
-        self.date_available_from = date_available_from
-        self.hotel_name = hotel_name
-        self.city_name = city_name
-        self.booked_dates = []
-        self.check_in = None
-        self.check_out = None
+        self.room_id = room_id #unique id for room
+        self.price = price #price of room
+        self.persons = persons #number of people who can fit in a room
+        self.date_available_from = date_available_from #what days can the room be booked
+        self.hotel_name = hotel_name #this room is located in this hotel
+        self.city_name = city_name #the hotel exists in this city
+        self.booked_dates = []  #list to stor dates room is booked for 
+        self.check_in = None #check in date (start with none)
+        self.check_out = None #check out date (start with none)
 
     def get_id(self) -> int:
+        #get room id
         return self.room_id
 
     def get_price(self) -> int:
+        #GET PRICE OF ROOM
         return self.price
 
     def get_persons(self) -> int:
+        #get value of how many people can be in room
         return self.persons
 
     def get_date_available_from(self) -> date:
+        #get what days the room is available 
         return self.date_available_from
 
     def get_booked_dates(self) -> List[date]:
+        #get list of the dates room is booked
         return self.booked_dates
 
     def set_id(self, room_id: int):
+        #set room id (unique)
         self.room_id = room_id
 
     def set_price(self, price: int):
+        #set price of room
         self.price = price
 
     def set_persons(self, persons: int):
+        #set umber of people in room
         self.persons = persons
 
     def set_date_available_from(self, date_available_from: date):
+        #et avaibale dates 
         self.date_available_from = date_available_from
 
     def set_check_in(self, check_in: date):
+        #set check in date
         self.check_in = check_in
 
     def set_check_out(self, check_out: date):
+        #set check out date 
         self.check_out = check_out
