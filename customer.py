ae52ae967bc3de98f5f0ebceb1a6803715954794
Jonathan Aghachi <aghachi3@gmail.com>
2024-07-25 16:33:53 -0700

    connect customer class to database

--- customer.py
+++ customer.py
@@ -1,3 +1,5 @@
+from reservation import Reservation
+
 class Customer:
     def __init__(self, name: str, email: str, persons: int, reservations: list):
         self.name = name
@@ -5,24 +7,49 @@ class Customer:
         self.persons = persons
         self.reservations = reservations
 
-    def getEmail(self) -> str:
-        return self.email
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "email": self.email,
+            "persons": self.persons,
+            "reservations": self.reservations
+        }
 
-    def getPersons(self) -> int:
-        return self.persons
+    @classmethod
+    async def from_dict(cls, data):
+        return cls(
+            name=data['name'],
+            email=data['email'],
+            persons=data['persons'],
+            reservations=data['reservations']
+        )
 
-    def getReservations(self) -> list:
-        return self.reservations
+    async def save_to_db(self, db):
+        await db.customers.update_one({"email": self.email}, {"$set": self.to_dict()}, upsert=True)
 
-    def setEmail(self, email: str):
-        self.email = email
+    @staticmethod
+    async def load_from_db(db, email):
+        data = await db.customers.find_one({"email": email})
+        if data:
+            return await Customer.from_dict(data)
+        return None
 
-    def setPersons(self, persons: int):
-        self.persons = persons
+    async def add_reservation(self, reservation: Reservation, db):
+        self.reservations.append(reservation.confirmation_number)
+        await self.save_to_db(db)
 
-    def setName(self, name: str):
-        self.name = name
+    async def review_reservation(self, confirmation_number: int, db):
+        reservation = await Reservation.load_from_db(db, confirmation_number)
+        if reservation:
+            return f"Name: {self.name}, 
+                    Dates: {reservation.get_dates()}, 
+                    Room Type: {'Suite' if reservation.room_id < 200 else 'Double' if reservation.room_id < 300 else 'Single'}, 
+                    Confirmation Number: {reservation.get_confirmation_number()}"
+        return "Not Found"
 
-    def setReservation(self, reservations: list):
-        self.reservations = reservations
+    async def check_reservation(self, confirmation_number: int, db):
+        reservation = await Reservation.load_from_db(db, confirmation_number)
+        if reservation:
+            return f"Reservation Found: Name: {self.name}, Room ID: {reservation.room_id}, Dates: {reservation.get_dates()}"
+        return "Not Found"
         
