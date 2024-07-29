308e14037bab518928492af0149646a8f8b13fba
Jonathan Aghachi <aghachi3@gmail.com>
2024-07-29 16:57:06 -0700

    commented out libaries not being used

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QDateEdit, QComboBox, QPushButton, QMainWindow, QMenu, QAction, QSizePolicy, QSpacerItem, QLineEdit
-from PyQt5.QtGui import QPixmap, QFont, QIcon
-from PyQt5.QtCore import Qt, QDate, QPoint
-from functools import partial
+from PyQt5.QtWidgets import QApplication
+#, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QDateEdit, QComboBox, QPushButton, QMainWindow, QMenu, QAction, QSizePolicy, QSpacerItem, QLineEdit
+#from PyQt5.QtGui import QPixmap, QFont, QIcon
+#from PyQt5.QtCore import Qt, QDate, QPoint
+#from functools import partial
 from MainWindow import MainWindow
 
 class main():
