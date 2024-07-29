77339e11bb3fa1a2cb8311a7e7190918317f4d07
Macy Varga <macylain@outlook.com>
2024-07-29 13:49:44 -0700

    big changes (thursday-sunday work-ish)

--- reservation.py
+++ reservation.py
@@ -1,69 +1,76 @@
 from datetime import date
 from typing import List, Any
+import random
 
+#note:: dictionary for data base xoxx
+#time to manage hotel reservations!!
 class Reservation:
-    #object
-    def __init__(self, customer: Any, email: str, persons: int, roomId: int, confirmationNumber: int, checkIn: date, checkOut: date):
-        self.customer = customer
-        #customer info
-        self.email = email #customer email
-        self.persons = persons #number of people for resi
-        self.roomId = roomId #ID for resi 
-        self.confirmationNumber = confirmationNumber #confirmation numb of resi
-        self.checkIn = checkIn #checkin date
-        self.checkOut = checkOut #checkout date 
-
-    def getEmail(self) -> str:
-        #get email of customer 
-        return self.email
-
-    def getPrice(self) -> int:
-        #get price of resi
-        # assume price calc logic is implemented somewhere else?
-        # holder for actual price logic
-        return 0
-
-    def getPersons(self) -> int:
-        #get number of people
-        return self.persons
-
-    def getDateAvailableFrom(self) -> date:
-        #get next available date for resi
-        # holer space for method
-        return date.today()
-
-    def getBookedDates(self) -> List[date]:
-        #get booked dates for resi
-        # holder space for method
-        return [self.checkIn, self.checkOut]
-
-    def setConfirmation(self, confirmationNumber: int) -> None:
-        #set confrimation number for resi
-        self.confirmationNumber = confirmationNumber
-
-    def setPersons(self, persons: int) -> int:
-        #set nmber of people for resi 
+    #what the resi entails!!!
+    def __init__(self, customer: Any, email: str, persons: int, room_id: int, check_in: date, check_out: date): #constructor method
+        #initialization 
+        #get cutomer info and their desired room and dates for hotel stay 
+        self.customer = customer 
+        self.email = email
         self.persons = persons
-        return self.persons
+        self.room_id = room_id
+        self.confirmation_number = random.randint(100000, 999999) #confirmation num will be random 
+        self.check_in = check_in
+        self.check_out = check_out
 
-    def setDateAvailableFrom(self, dateAvailableFrom: date) -> None:
-        #holder space for method
-        #set next available date for resi
-        pass
 
-    def modifyReservation(self, confirmationNumber: int, modifyCancelReserve: str) -> None:
-        self.confirmationNumber = confirmationNumber
-        # holder space for method
-        #change or cancel resi based of confirmation number and user action to do so 
+#convert to dictionary
+    def to_dict(self):
+        #change resi object into dictionary format
+            #do this for da database 
+        return {
+         
+            "customer": self.customer,
+            "email": self.email,
+            "persons": self.persons,
+            "room_id": self.room_id,
+            "confirmation_number": self.confirmation_number,
+            "check_in": self.check_in,
+            "check_out": self.check_out
+        }
 
-    def cancelReservation(self) -> None:
-        # holder method for cancel reservation logic
-        pass
+#create resi from dictionary
+    @classmethod
+    async def from_dict(cls, data):
+#class method makes Reservation instance fromdictionary 
+#asynchronous operation
+        return cls(
+            customer=data['customer'],
+            email=data['email'],
+            persons=data['persons'],
+            room_id=data['room_id'],
+            check_in=data['check_in'],
+            check_out=data['check_out']
+        )
 
-    def reserveRoom(self, id: int, price: int, persons: int, dateAvailableFrom: date, bookedDates: List[date]) -> None:
-        self.roomId = id
-        # Placeholder for reserve room logic
-        #reserve a room with given details
-        self.setPersons(persons)
-        self.setDateAvailableFrom(dateAvailableFrom)
-        self.bookedDates = bookedDates
+
+#Save reservation to database 
+    async def save_to_db(self, db):
+        #add doc if it isn't there already 
+        await db.reservations.update_one({"confirmation_number": self.confirmation_number}, {"$set": self.to_dict()}, upsert=True)
+
+#load resi from database 
+    @staticmethod
+    async def load_from_db(db, confirmation_number):
+        #if resi is found then create instance 
+        data = await db.reservations.find_one({"confirmation_number": confirmation_number})
+        if data:
+            return await Reservation.from_dict(data)
+        return None
+
+#reserve room
+    async def reserve_room(self, db, rooms):
+        room = await Room.load_from_db(db, self.room_id)
+        #load room from database 
+        if room and await room.book_room(self.check_in, self.check_out, db):
+            #book by dates 
+            await self.save_to_db(db)
+            #save resi to database if booked
+            return True
+        #successful
+        return False
+    #not successful 
--- room.py
+++ room.py
@@ -1,86 +1,115 @@
 from datetime import date
 from typing import List
+import asyncio
 
 #note about rooms:
 ##SUITE: persons 1-4, $200 per night
 ##DOUBLE: persons 1-4, $125 per night
 ##SINGLE: persons 1, $75 per night
+
+
 class Room:
     def __init__(self, room_id: int, price: int, persons: int, date_available_from: date, check_in: date = None, check_out: date = None):
-        self.room_id = room_id #unique id for room
+       #initializes attributes 
+       self.room_id = room_id #unique id for room
         self.price = price #price of room
         self.persons = persons #number of people who can fit in a room
         self.date_available_from = date_available_from #what days can the room be booked
         self.booked_dates = []  #list to stor dates room is booked for 
+       #initialize empty list to store booked dates 
         self.check_in = check_in #check in date (start with none)
         self.check_out = check_out #check out date (start with none)
         
-    def get_id(self) -> int:
-        #get room id
-        return self.room_id
+#convert to and from dictionary 
+    def to_dict(self):
+         #converts a room object into a dictionary
+        return {
+            "room_id": self.room_id,
+            "price": self.price,
+            "persons": self.persons,
+            "date_available_from": self.date_available_from,
+            "booked_dates": self.booked_dates,
+            "check_in": self.check_in,
+            "check_out": self.check_out
+        }
+   
 
-    def get_price(self) -> int:
-        #GET PRICE OF ROOM
-        return self.price
+    @classmethod
+    async def from_dict(cls, data):
+        #creates a room object from dictionary
+        return cls(
+            room_id=data['room_id'],
+            price=data['price'],
+            persons=data['persons'],
+            date_available_from=data['date_available_from'],
+            check_in=data.get('check_in'),
+            check_out=data.get('check_out')
+        )
+    
+#database operationz
+    async def save_to_db(self, db):
+        #saves the room object to database
+        #inserts document if it doesnt exist
+        await db.rooms.update_one({"room_id": self.room_id}, {"$set": self.to_dict()}, upsert=True)
 
-    def get_persons(self) -> int:
-        #get value of how many people can be in room
-        return self.persons
+    @staticmethod
+    async def load_from_db(db, room_id):
+        #load a room object from database using room id
+        data = await db.rooms.find_one({"room_id": room_id})
+        if data:
+            return await Room.from_dict(data)
+        return None
 
-    def get_date_available_from(self) -> date:
-        #get what days the room is available 
-        return self.date_available_from
 
-    def get_booked_dates(self) -> List[date]:
-        #get list of the dates room is booked
-        return self.booked_dates
 
-    def set_id(self, room_id: int):
-        #set room id (unique)
-        self.room_id = room_id
+    @staticmethod
+    async def load_all_from_db(db):
+        #loads all rooms from database 
+        rooms = []
+        async for data in db.rooms.find():
+            rooms.append(await Room.from_dict(data))
+        return rooms
 
-    def set_price(self, price: int):
-        #set price of room
-        self.price = price
+#booking a room
+    async def book_room(self, check_in: date, check_out: date, db):
+        #book the room if it is available for the given dates 
+        #check availability 
+        #add dates to the booked dates list then save updated data in database 
+        if self.is_available(check_in, check_out):
+          
+            current_date = check_in
+            while current_date < check_out:
+                self.booked_dates.append(current_date)
+                current_date += timedelta(days=1)
+            await self.save_to_db(db)
+            return True
+        return False
 
-    def set_persons(self, persons: int):
-        #set umber of people in room
-        self.persons = persons
 
-    def set_date_available_from(self, date_available_from: date):
-        #et avaibale dates 
-        self.date_available_from = date_available_from
 
-    def set_check_in(self, check_in: date):
-        #set check in date
-        self.check_in = check_in
-
-    def set_check_out(self, check_out: date):
-        #set check out date 
-        self.check_out = check_out
-
-    def is_available(self, check_in: date, check_out,:date) -> bool:
-        #if available during those dates 
+    def is_available(self, check_in: date, check_out: date) -> bool:
+          #check if room is available for the given dates by ensurinf there are no overlapping dates in booked _dates
         for booked_date in self.booked_dates:
-                if (check_in <= booked_date < check_out) or (booked_date <= check_in <check_out):
-                    return False
-                return True
-        
+            if (check_in <= booked_date < check_out) or (booked_date <= check_in < check_out):
+                return False
+        return True
+    
 
-### adding room descriptions 
-rooms = []
-#single
-for i in range(100, 105):
-    rooms.append(Room(room_id=i, price=200, persons=4, date_available_from=date.today()))
+#initiallizing rooms
+async def initialize_rooms(db):
+    #create and save rooms to da database based on predefined types and IDSs
+    #set of rooms
+    room_types = [
+        {"type": "Suite", "price": 200, "persons": 4, "range": range(100, 105)},
+        {"type": "Double", "price": 125, "persons": 4, "range": range(200, 205)},
+        {"type": "Single", "price": 75, "persons": 1, "range": range(300, 305)}
+    ]
+    for room_type in room_types:
+        for room_id in room_type["range"]:
+            room = Room(room_id, room_type["price"], room_type["persons"], date.today())
+            await room.save_to_db(db)
 
- #double
-for i in range(200, 205):
-    rooms.append(Room(room_id=i, price=125, persons=4, date_available_from=date.today()))
 
-#single
-for i in range(300, 305):
-    rooms.append(Room(room_id=i, price=75, persons=1, date_available_from=date.today()))
-
-#room verify output
-for room in rooms:
-    print(f"Room ID: {room.get_id()}, Type: {'Suite' if room.get_price() == 200 else 'Double' if room.get_price() == 125 else 'Single'}, Price: {room.get_price()}, Max Persons: {room.get_persons()}, Available From: {room.get_date_available_from()}")
+#execute the queen... haha jk,, main execute
+db = asyncio.run(get_database())
+asyncio.run(initialize_rooms(db)
