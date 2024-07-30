d02a378fbf1a3be96ac22c0e6c1d96b5d0d64688
k1mmari <kwuffle133@gmail.com>
2024-07-30 13:58:57 -0700

    finished booking details window

--- RoomBookingDetailsWindow.py
+++ RoomBookingDetailsWindow.py
@@ -21,3 +21,68 @@ class RoomBookingDetailsWindow(QWidget):
         room_image_label.setPixmap(pixmap)
         room_image_label.setAlignment(Qt.AlignCenter)
         main_layout.addWidget(room_image_label)
+
+        # Booking details
+        details_label = QLabel(f"Booking for {adults} people\nCheck-in: {checkin_date.toString('MMM d, yyyy')} - Check-out: {checkout_date.toString('MMM d, yyyy')}")
+        details_label.setStyleSheet("font-size: 18px; color: black;")
+        details_label.setAlignment(Qt.AlignCenter)
+        main_layout.addWidget(details_label)
+
+        # Input the name
+        name_label = QLabel("Enter the name for the booking: ")
+        name_label.setStyleSheet("font-size: 18px; color: black;")
+        name_label.setAlignment(Qt.AlignCenter)
+        main_layout.addWidget(name_label)
+
+        self.name_input = QLineEdit(self)
+        self.name_input.setPlaceholderText("Your Name")
+        main_layout.addWidget(self.name_input)
+
+        # Buttons
+        buttons_layout = QHBoxLayout()
+        
+        # Book-button
+        book_button = QPushButton("Book")
+        book_button.setStyleSheet("""
+            QPushButton {
+                background-color: #2B1C19;
+                color: white;
+                font-size: 16px;
+                font-weight: bold;
+                padding: 5px;
+                border-radius: 5px;
+            }
+            QPushButton:hover {
+                background-color: #E5D5C3;
+                color: black;
+            }
+        """)
+        book_button.clicked.connect(lambda: self.open_booking_confirmation(room_name, checkin_date, checkout_date, adults))
+        buttons_layout.addWidget(book_button)
+
+        # Back-button
+        back_button = QPushButton("Back")
+        back_button.setStyleSheet("""
+            QPushButton {
+                background-color: #2B1C19;
+                color: white;
+                font-size: 16px;
+                font-weight: bold;
+                padding: 5px;
+                border-radius: 5px;
+            }
+            QPushButton:hover {
+                background-color: #E5D5C3;
+                color: black;
+            }
+        """)
+        back_button.clicked.connect(self.close)  # Close the current window
+        buttons_layout.addWidget(back_button)
+
+        main_layout.addLayout(buttons_layout)
+
+    def open_booking_confirmation(self, room_name, checkin_date, checkout_date, adults):
+        user_name = self.name_input.text()
+        self.confirmation_window = BookingConfirmationWindow(room_name, checkin_date, checkout_date, adults, user_name)
+        self.confirmation_window.setWindowModality(Qt.ApplicationModal)
+        self.confirmation_window.show()
