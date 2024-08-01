9a9d85b7d4b4e97f45c3de570d7a11a5d931bc48
Jonathan Aghachi <aghachi3@gmail.com>
2024-08-01 00:29:21 -0700

    simplified reservation class

--- reservation.py
+++ reservation.py
@@ -1,67 +1,52 @@
 from datetime import date
+from msilib.schema import CustomAction
 from typing import List, Any
+from customer import Customer
 
 class Reservation:
     #object
     def __init__(self):
-        self.customer = None
+        self.customer = Customer()
         #customer info
         self.roomId = None #ID for resi 
         self.confirmationNumber = None #confirmation numb of resi
+        self.price = 0
         self.checkIn = None #checkin date
         self.checkOut = None #checkout date 
 
+    def getCustomer(self) -> Customer:
+        #get customer 
+        return self.customer
+    
     def getEmail(self) -> str:
         #get email of customer 
-        return self.email
+        return self.customer.email
 
     def getPrice(self) -> int:
         #get price of resi
-        # assume price calc logic is implemented somewhere else?
-        # holder for actual price logic
-        return 0
+        return self.price
 
     def getPersons(self) -> int:
         #get number of people
-        return self.persons
-
-    def getDateAvailableFrom(self) -> date:
-        #get next available date for resi
-        # holer space for method
-        return date.today()
-
-    def getBookedDates(self) -> List[date]:
-        #get booked dates for resi
-        # holder space for method
-        return [self.checkIn, self.checkOut]
+        return self.customer.persons
+    
+    def setCustomer(self, customer) -> None:
+        self.customer = customer
 
     def setConfirmation(self, confirmationNumber: int) -> None:
         #set confrimation number for resi
         self.confirmationNumber = confirmationNumber
 
-    def setPersons(self, persons: int) -> int:
-        #set nmber of people for resi 
-        self.persons = persons
-        return self.persons
-
-    def setDateAvailableFrom(self, dateAvailableFrom: date) -> None:
-        #holder space for method
-        #set next available date for resi
-        pass
-
-    def modifyReservation(self, confirmationNumber: int, modifyCancelReserve: str) -> None:
+    def modifyReservation(self, confirmationNumber: int) -> None:
         self.confirmationNumber = confirmationNumber
         # holder space for method
         #change or cancel resi based of confirmation number and user action to do so 
 
-    def cancelReservation(self) -> None:
+    def cancelReservation(self,confirmationNumber: int,) -> None:
         # holder method for cancel reservation logic
         pass
 
-    def reserveRoom(self, id: int, price: int, persons: int, dateAvailableFrom: date, bookedDates: List[date]) -> None:
+    def reserveRoom(self, ) -> None:
         self.roomId = id
         # Placeholder for reserve room logic
         #reserve a room with given details
-        self.setPersons(persons)
-        self.setDateAvailableFrom(dateAvailableFrom)
-        self.bookedDates = bookedDates
